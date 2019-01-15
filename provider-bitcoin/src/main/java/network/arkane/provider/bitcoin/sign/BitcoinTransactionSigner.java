e3d531cff401a8a55fddfe6abd346d905090cb1e
Quinten De Swaef <quintendeswaef@gmail.com>
2019-01-15 10:59:26 +0100

    sending bitcoin

--- provider-bitcoin/src/main/java/network/arkane/provider/bitcoin/sign/BitcoinTransactionSigner.java
+++ provider-bitcoin/src/main/java/network/arkane/provider/bitcoin/sign/BitcoinTransactionSigner.java
@@ -12,7 +12,6 @@ import network.arkane.provider.exceptions.ArkaneException;
 import network.arkane.provider.sign.Signer;
 import network.arkane.provider.sign.domain.Signature;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.Base64InputStream;
 import org.apache.commons.codec.binary.Hex;
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.Coin;
@@ -30,11 +29,8 @@ import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.script.Script;
 import org.bitcoinj.script.ScriptBuilder;
 import org.bitcoinj.wallet.Protos;
-import org.bitcoinj.wallet.UnreadableWalletException;
-import org.bitcoinj.wallet.Wallet;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayInputStream;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -50,34 +46,43 @@ public class BitcoinTransactionSigner implements Signer<BitcoinTransactionSignab
     }
 
     @Override
-    public Signature createSignature(BitcoinTransactionSignable signable, BitcoinSecretKey key) {
-        final DumpedPrivateKey privateKey = DumpedPrivateKey.fromBase58(NETWORK_PARAMS, "");
-        final Address fromAddress = new Address(NETWORK_PARAMS, privateKey.getKey().getPubKeyHash());
-
+    public Signature createSignature(BitcoinTransactionSignable signable, BitcoinSecretKey secretKey) {
+        final ECKey key = DumpedPrivateKey.fromBase58(NETWORK_PARAMS, "92JYtSuKyhrG1fVgtBXUQgT8yNGs6XFFCjz1XLCwg8jFM95GHB6").getKey();
+        final Address fromAddress = new Address(NETWORK_PARAMS, key.getPubKeyHash());
         try {
             final Transaction tx = new Transaction(NETWORK_PARAMS);
             final Coin amount = Coin.valueOf(signable.getSatoshiValue().longValue());
 
-            tx.addOutput(amount, Address.fromBase58(NETWORK_PARAMS, privateKey.getKey().getPublicKeyAsHex()));
+            tx.addOutput(amount, new Address(NETWORK_PARAMS, key.getPubKeyHash()));
             addInputsToTransaction(Address.fromBase58(NETWORK_PARAMS, fromAddress.toBase58()), tx, fetchUnspents(fromAddress), amount.value);
-            signInputsOfTransaction(Address.fromBase58(NETWORK_PARAMS, fromAddress.toBase58()), tx, privateKey.getKey());
+            signInputsOfTransaction(Address.fromBase58(NETWORK_PARAMS, fromAddress.toBase58()), tx, key);
             tx.verify();
             tx.setPurpose(Transaction.Purpose.USER_PAYMENT);
             return network.arkane.provider.sign.domain.TransactionSignature
                     .signTransactionBuilder()
                     .signedTransaction(Hex.encodeHexString(tx.bitcoinSerialize()))
                     .build();
+        } catch (final ArkaneException ex) {
+            log.error(ex.getMessage());
+            throw ex;
         } catch (final Exception ex) {
             log.error(ex.getMessage());
             throw ArkaneException.arkaneException()
                                  .errorCode("bitcoin.signing-error")
-                                 .message(String.format("An error occurred trying to sign the bitcoin transaction %s", ex.getMessage()))
+                                 .message(String.format("An error occurred trying to sign the bitcoin transaction: %s", ex.getMessage()))
                                  .build();
         }
     }
 
     private List<Unspent> fetchUnspents(final Address address) {
-        return unspentService.getUnspentForAddress(address);
+        final List<Unspent> unspentForAddress = unspentService.getUnspentForAddress(address);
+        if (unspentForAddress.isEmpty()) {
+            throw ArkaneException.arkaneException()
+                                 .errorCode("bitcoin.signing-inputs")
+                                 .message(String.format("The account you're trying to sign with doesn't have valid inputs to send the transaction"))
+                                 .build();
+        }
+        return unspentForAddress;
     }
 
     private void signInputsOfTransaction(Address sourceAddress, Transaction tx, ECKey key) {
@@ -112,6 +117,13 @@ public class BitcoinTransactionSigner implements Signer<BitcoinTransactionSignab
                 break;
             }
         }
+        if (gatheredAmount < requiredAmount) {
+            throw ArkaneException.arkaneException()
+                                 .errorCode("bitcoin.not-enough-funds")
+                                 .message(String.format("Not enough funds to send the transaction"))
+                                 .build();
+        }
+
         if (gatheredAmount > requiredAmount) {
             //return change to sender, in real life it should use different address
             tx.addOutput(Coin.valueOf((gatheredAmount - requiredAmount)), sourceAddress);
--- provider-bitcoin/src/test/java/network/arkane/provider/bitcoin/sign/BitcoinTransactionSignerTest.java
+++ provider-bitcoin/src/test/java/network/arkane/provider/bitcoin/sign/BitcoinTransactionSignerTest.java
@@ -1,47 +1,87 @@
 package network.arkane.provider.bitcoin.sign;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
-import org.apache.commons.codec.binary.Base64;
+import network.arkane.provider.bitcoin.secret.generation.BitcoinSecretKey;
+import network.arkane.provider.bitcoin.unspent.Unspent;
+import network.arkane.provider.bitcoin.unspent.UnspentService;
+import network.arkane.provider.sign.domain.Signature;
+import network.arkane.provider.sign.domain.TransactionSignature;
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.Base58;
+import org.bitcoinj.core.ECKey;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.net.URL;
-import java.util.HashMap;
+import java.math.BigInteger;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class BitcoinTransactionSignerTest {
 
+    private BitcoinTransactionSigner bitcoinTransactionSigner;
+    private UnspentService unspentService;
+
+
+    @BeforeEach
+    void setUp() {
+        unspentService = mock(UnspentService.class);
+        bitcoinTransactionSigner = new BitcoinTransactionSigner(unspentService);
+    }
+
     @Test
-    void blah() throws Throwable {
-        //        JsonRpcClient client = new JsonRpcClient(new Transport() {
-        //            CloseableHttpClient httpClient = HttpClients.createDefault();
-        //            @NotNull
-        //            @Override
-        //            public String pass(@NotNull String request) throws IOException {
-        //                // Used Apache HttpClient 4.3.1 as an example
-        //                HttpPost post = new HttpPost("");
-        //                post.setEntity(new StringEntity(request, Charsets.UTF_8));
-        //                post.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.JSON_UTF_8.toString());
-        //                try (CloseableHttpResponse httpResponse = httpClient.execute(post)) {
-        //                    return EntityUtils.toString(httpResponse.getEntity(), Charsets.UTF_8);
-        //                }
-        //            }
-        //        });
-        //
-        //        client.createRequest()
-        //
-        //              .id(100)
-        //              .method("getblockchaininfo")
-        //              .returnAsMap(HashMap.class, String.class)
-        //              .execute();
+    void signsCorrectly() {
+        when(unspentService.getUnspentForAddress(any(Address.class))).thenReturn(Arrays.asList(
+                Unspent
+                        .builder()
+                        .amount(14482185)
+                        .scriptPubKey("76a91464d12bfa319ec47fe040bf6d2dbb3013d85f552588ac")
+                        .txId("0f496014bbb15adfd924a2b2ce8c16a4b6076240e4428d96b344d66adbb14c0b")
+                        .vOut(1).build()));
+        Signature signature = bitcoinTransactionSigner.createSignature(BitcoinTransactionSignable.builder()
+                                                                                                 .satoshiValue(BigInteger.ONE)
+                                                                                                 .address("mhSwuar1U3Hf6phh2LMkFefkjCgFt8Xg5H")
+                                                                                                 .build(),
+                                                                       BitcoinSecretKey.builder()
+                                                                                       .key(ECKey.fromPrivate(Base58.decode("92JYtSuKyhrG1fVgtBXUQgT8yNGs6XFFCjz1XLCwg8jFM95GHB6")))
+                                                                                       .build());
+
+        assertThat(signature).isInstanceOf(TransactionSignature.class);
+        assertThat(((TransactionSignature) signature).getSignedTransaction()).isEqualTo("01000000010b4cb1db6ad644b3968d42e4406207b6a4168cceb2a224d9df5ab1bb1460490f010000008a4730440220711a81110ec055d072c5769a2cde6aefc9024234670b4c46fe872b20e457daee0220448d49888fa817f73cd05e418a7bf42ed2b4c8b364c48cca75bde8ce9a509d9781410496e59446aed552e60fb29b6a9c6c71d7c1b38b8396e67d9940dbb867a70e314c591b20a0e59e36259439e1d0b6ae16975ca7097103d8d1ac987a02d121b5cd16ffffffff0201000000000000001976a91464d12bfa319ec47fe040bf6d2dbb3013d85f552588ac6874db00000000001976a91464d12bfa319ec47fe040bf6d2dbb3013d85f552588ac00000000");
+    }
+
+    @Test
+    void cantSignWithoutUnspents() {
+      assertThatThrownBy(() -> bitcoinTransactionSigner.createSignature(BitcoinTransactionSignable.builder()
+                                                                                                 .satoshiValue(BigInteger.ONE)
+                                                                                                 .address("mhSwuar1U3Hf6phh2LMkFefkjCgFt8Xg5H")
+                                                                                                 .build(),
+                                                                       BitcoinSecretKey.builder()
+                                                                                       .key(ECKey.fromPrivate(Base58.decode("92JYtSuKyhrG1fVgtBXUQgT8yNGs6XFFCjz1XLCwg8jFM95GHB6")))
+                                                                                       .build())).hasMessageContaining("The account you're trying to sign with doesn't have valid inputs to send the transaction");
+
+    }
 
 
-        HashMap<String, String> headers = new HashMap<>();
-        String auth = new String(Base64.encodeBase64("arkane:dk36EsfUu2ze7PjQdj5zTFY9fEMMPb4v63c9W2azctq4dQtERP".getBytes()));
-        headers.put("Authorization", "Basic " + auth);
-        JsonRpcHttpClient client = new JsonRpcHttpClient(new ObjectMapper(),
-                                                         new URL("http://95.216.159.193:18332"), headers);
-
-        client.invoke("importprivkey", new Object[] {"Kxt4AVyCSCXnpDWhRS5JpRxw2PMkGn25yY4JxCJL3KzwUj2JJhrE", "davy_test"});
+    @Test
+    void notEnoughFunds() {
+        when(unspentService.getUnspentForAddress(any(Address.class))).thenReturn(Arrays.asList(
+                Unspent
+                        .builder()
+                        .amount(14482185)
+                        .scriptPubKey("76a91464d12bfa319ec47fe040bf6d2dbb3013d85f552588ac")
+                        .txId("0f496014bbb15adfd924a2b2ce8c16a4b6076240e4428d96b344d66adbb14c0b")
+                        .vOut(1).build()));
+        assertThatThrownBy(() -> bitcoinTransactionSigner.createSignature(BitcoinTransactionSignable.builder()
+                                                                                              .satoshiValue(BigInteger.valueOf(14482195))
+                                                                                              .address("mhSwuar1U3Hf6phh2LMkFefkjCgFt8Xg5H")
+                                                                                              .build(),
+                                                                    BitcoinSecretKey.builder()
+                                                                                    .key(ECKey.fromPrivate(Base58.decode("92JYtSuKyhrG1fVgtBXUQgT8yNGs6XFFCjz1XLCwg8jFM95GHB6")))
+                                                                                    .build())).hasMessageContaining("Not enough funds to send the transaction");
 
     }
 }
