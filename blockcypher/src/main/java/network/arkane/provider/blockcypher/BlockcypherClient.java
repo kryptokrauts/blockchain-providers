c48187e04a60cec0789f64b3620edbf6087ebc69
Quinten De Swaef <quintendeswaef@gmail.com>
2019-01-18 15:47:15 +0100

    blockcypher unspents

---
+++ blockcypher/src/main/java/network/arkane/provider/blockcypher/BlockcypherClient.java
@@ -0,0 +1,17 @@
+package network.arkane.provider.blockcypher;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "blockcypher-client", url = "https://api.blockcypher.com/v1", decode404 = true)
+public interface BlockcypherClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{coin}/{network}/addr/{address}?unspentOnly=true")
+    Object getUnspents(@RequestHeader("User-Agent") String userAgent,
+                       @PathVariable("network") final String network,
+                       @PathVariable("coin") final String coin,
+                       @PathVariable("address") final String address);
+}
