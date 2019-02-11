57946218b1e6575d5a20c6093accc9d7a563c1a7
Quinten De Swaef <quintendeswaef@gmail.com>
2019-02-11 09:18:30 +0100

    fix tron autoconfiguration

--- provider-tron/src/main/java/network/arkane/provider/tron/TronAutoConfiguration.java
+++ provider-tron/src/main/java/network/arkane/provider/tron/TronAutoConfiguration.java
@@ -1,6 +1,9 @@
 package network.arkane.provider.tron;
 
+import io.grpc.Grpc;
+import network.arkane.provider.tron.grpc.GrpcClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.tron.core.Constant;
 import org.tron.core.Wallet;
@@ -8,7 +11,12 @@ import org.tron.core.Wallet;
 @Configuration
 public class TronAutoConfiguration {
 
+    private final String fullNode;
+    private final String solidityNode;
+
     public TronAutoConfiguration(final @Value("${network.arkane.tron.fullnode.url:'grpc.trongrid.io:50051'}") String fullNode, final @Value("${network.arkane.tron.soliditynode.url:'grpc.trongrid.io:50052'}") String solidityNode, final String environment) {
+        this.fullNode = fullNode;
+        this.solidityNode = solidityNode;
         if ("testnet".equals(environment)) {
             Wallet.setAddressPreFixByte(Constant.ADD_PRE_FIX_BYTE_TESTNET);
             Wallet.setAddressPreFixString(Constant.ADD_PRE_FIX_STRING_TESTNET);
@@ -16,5 +24,10 @@ public class TronAutoConfiguration {
             Wallet.setAddressPreFixByte(Constant.ADD_PRE_FIX_BYTE_MAINNET);
             Wallet.setAddressPreFixString(Constant.ADD_PRE_FIX_STRING_MAINNET);
         }
+    }
+
+    @Bean
+    public GrpcClient client() {
+        return new GrpcClient(fullNode, solidityNode);
     }
 }
