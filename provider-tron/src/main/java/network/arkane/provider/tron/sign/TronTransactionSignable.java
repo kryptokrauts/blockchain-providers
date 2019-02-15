08c86ac4b2ab0382f03092fd71ee6c197c1f301e
Quinten De Swaef <quintendeswaef@gmail.com>
2019-02-15 09:13:42 +0100

    added data to tx

--- provider-tron/src/main/java/network/arkane/provider/tron/sign/TronTransactionSignable.java
+++ provider-tron/src/main/java/network/arkane/provider/tron/sign/TronTransactionSignable.java
@@ -13,11 +13,14 @@ public class TronTransactionSignable implements Signable {
 
     private Long amount;
     private String to;
+    private String data;
 
     @Builder
     public TronTransactionSignable(final String to,
-                                   final Long amount) {
+                                   final Long amount,
+                                   final String data) {
         this.to = to;
         this.amount = amount;
+        this.data = data;
     }
 }
