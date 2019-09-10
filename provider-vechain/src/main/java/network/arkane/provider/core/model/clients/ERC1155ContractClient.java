949c5fd7c862bf4218e21311cc9aacc2a3795d68
Quinten De Swaef <quintendeswaef@gmail.com>
2019-09-10 17:14:29 +0200

    erc1155client'

--- provider-vechain/src/main/java/network/arkane/provider/core/model/clients/ERC1155ContractClient.java
+++ provider-vechain/src/main/java/network/arkane/provider/core/model/clients/ERC1155ContractClient.java
@@ -11,13 +11,14 @@ import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.DynamicArray;
 import org.web3j.abi.datatypes.Function;
 import org.web3j.abi.datatypes.NumericType;
-import org.web3j.abi.datatypes.Type;
 import org.web3j.abi.datatypes.generated.Uint256;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class ERC1155ContractClient extends TransactionClient {
@@ -36,22 +37,29 @@ public class ERC1155ContractClient extends TransactionClient {
     }
 
     @SneakyThrows
-    public static List<BigInteger> balanceOfBatch(final String contractAddress,
-                                                  final List<String> owners,
-                                                  final List<BigInteger> ids) {
+    public static List<BigInteger> balanceOf(final String contractAddress,
+                                             final String owner,
+                                             final List<BigInteger> ids) {
         final AbiDefinition abiDefinition = ERC1155Contract.defaultERC1155Contract.findAbiDefinition("balanceOfBatch");
-        final ContractCall call = ERC1155Contract.buildCall(abiDefinition, owners.toArray(), ids.toArray());
+        final String[] owners = IntStream.range(0, ids.size())
+                                         .mapToObj(x -> owner)
+                                         .collect(Collectors.toList())
+                                         .toArray(new String[] {});
+        final ContractCall call = ERC1155Contract.buildCall(abiDefinition,
+                                                            owners,
+                                                            ids.toArray());
         final String data = callContract(call, Address.fromHexString(contractAddress), Revision.BEST).getData();
         Function f = new Function("balanceOfBatch",
                                   Arrays.asList(new DynamicArray<org.web3j.abi.datatypes.Address>(
-                                          owners.stream().map(org.web3j.abi.datatypes.Address::new).collect(Collectors.toList())
+                                          Arrays.stream(owners)
+                                                .map(org.web3j.abi.datatypes.Address::new).collect(Collectors.toList())
                                   ), new DynamicArray<>(
                                           ids.stream().map(Uint256::new).collect(Collectors.toList()))),
                                   Arrays.asList(new TypeReference<DynamicArray<Uint256>>() {}));
-        final List<Type> decode = FunctionReturnDecoder.decode(data, f.getOutputParameters());
-        return decode.stream()
-                     .map(x -> (Uint256) x)
-                     .map(NumericType::getValue)
-                     .collect(Collectors.toList());
+        return ((ArrayList<Uint256>) FunctionReturnDecoder.decode(data, f.getOutputParameters())
+                                                          .get(0).getValue())
+                .stream()
+                .map(NumericType::getValue)
+                .collect(Collectors.toList());
     }
 }
