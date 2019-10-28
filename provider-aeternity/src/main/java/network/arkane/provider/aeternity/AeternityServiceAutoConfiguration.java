9a100b0086e4c3e2e47fbae91c5ac67f3d3938a4
Marco Walz <walz.marcoo@gmail.com>
2019-10-29 00:37:28 +0100

    use release v2.0.0 and point to testnet-endpoints by default

--- provider-aeternity/src/main/java/network/arkane/provider/aeternity/AeternityServiceAutoConfiguration.java
+++ provider-aeternity/src/main/java/network/arkane/provider/aeternity/AeternityServiceAutoConfiguration.java
@@ -12,28 +12,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AeternityServiceAutoConfiguration {
 
-    @Value("${network.arkane.aeternity.node.api.baseUrl:https://sdk-testnet.aepps.com/v2}")
-    String baseUrl;
-    @Value("${network.arkane.aeternity.compiler.api.baseUrl:https://compiler.aepps.com}")
-    String compilerBaseUrl;
-    @Value("${network.arkane.aeternity.network:TESTNET}")
-    Network network;
-    @Value("${network.arkane.aeternity.vm:FATE}")
-    VirtualMachine targetVM;
+  @Value("${network.arkane.aeternity.node.api.baseUrl:https://sdk-testnet.aepps.com}")
+  String baseUrl;
+  @Value("${network.arkane.aeternity.compiler.api.baseUrl:https://compiler.aepps.com}")
+  String compilerBaseUrl;
+  @Value("${network.arkane.aeternity.aeternal.api.baseUrl:https://testnet.aeternal.io}")
+  String aeternalBaseUrl;
+  @Value("${network.arkane.aeternity.network:TESTNET}")
+  Network network;
+  @Value("${network.arkane.aeternity.vm:FATE}")
+  VirtualMachine targetVM;
 
-    @Bean(name = "aeternity-configuration")
-    public AeternityServiceConfiguration aeternityConfiguration() {
-        return AeternityServiceConfiguration.configure()
-                .baseUrl(baseUrl)
-                .compilerBaseUrl(compilerBaseUrl)
-                .network(network)
-                .targetVM(targetVM)
-                .compile();
-    }
+  @Bean(name = "aeternity-configuration")
+  public AeternityServiceConfiguration aeternityConfiguration() {
+    return AeternityServiceConfiguration.configure()
+        .baseUrl(baseUrl)
+        .compilerBaseUrl(compilerBaseUrl)
+        .aeternalBaseUrl(aeternalBaseUrl)
+        .network(network)
+        .targetVM(targetVM)
+        .compile();
+  }
 
-    @Bean(name = "aeternity-service")
-    public AeternityService aeternityService() {
-        return new AeternityServiceFactory()
-                .getService(aeternityConfiguration());
-    }
+  @Bean(name = "aeternity-service")
+  public AeternityService aeternityService() {
+    return new AeternityServiceFactory()
+        .getService(aeternityConfiguration());
+  }
 }
