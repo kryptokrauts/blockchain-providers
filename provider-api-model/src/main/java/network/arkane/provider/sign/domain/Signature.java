66f9dd0ab4591f5497dcd4388b3b178d8d482e64
Nico Peeters <nico@itwhiz.be>
2019-11-25 15:07:29 +0100

    Add type to Signature (enabling jackson deserialization)

--- provider-api-model/src/main/java/network/arkane/provider/sign/domain/Signature.java
+++ provider-api-model/src/main/java/network/arkane/provider/sign/domain/Signature.java
@@ -3,6 +3,7 @@ package network.arkane.provider.sign.domain;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXISTING_PROPERTY;
@@ -13,6 +14,7 @@ import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.EXISTING_PROPERTY
                        @JsonSubTypes.Type(value = SubmittedAndSignedTransactionSignature.class, name = SignatureType.Values.SUBMITTED_AND_SIGNED_TRANSACTION_SIGNATURE),
                        @JsonSubTypes.Type(value = TransactionSignature.class, name = SignatureType.Values.TRANSACTION_SIGNATURE),
                })
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Signature {
