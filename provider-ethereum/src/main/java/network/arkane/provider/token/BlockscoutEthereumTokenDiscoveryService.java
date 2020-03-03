5b1c33938753ce2f5b5149809acf2c0da4d63ea7
davyvanroy <davy.van.roy@gmail.com>
2020-03-03 16:07:32 +0100

    ARKANE-1561 some updates

--- provider-ethereum/src/main/java/network/arkane/provider/token/BlockscoutEthereumTokenDiscoveryService.java
+++ provider-ethereum/src/main/java/network/arkane/provider/token/BlockscoutEthereumTokenDiscoveryService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component
+@Component("blockscoutEthereumTokenDiscoveryService")
 @ConditionalOnBean(name = "ethereumBlockscoutClient")
 public class BlockscoutEthereumTokenDiscoveryService implements NativeTokenDiscoveryService {
 
