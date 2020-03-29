ff360b6719791c05682712d37cb2b01708842ff5
davyvanroy <davy.van.roy@gmail.com>
2020-03-29 14:06:29 +0200

    add logging

--- provider-ethereum/src/main/java/network/arkane/provider/balance/EthereumBalanceGateway.java
+++ provider-ethereum/src/main/java/network/arkane/provider/balance/EthereumBalanceGateway.java
@@ -52,6 +52,7 @@ public class EthereumBalanceGateway extends BalanceGateway {
                           .decimals(18)
                           .build();
         } catch (final Exception ex) {
+            log.error("Error getting balance for account", ex);
             throw ArkaneException.arkaneException()
                                  .message(String.format("Unable to get the balance for the specified account (%s)", account))
                                  .errorCode("web3.internal-error")
