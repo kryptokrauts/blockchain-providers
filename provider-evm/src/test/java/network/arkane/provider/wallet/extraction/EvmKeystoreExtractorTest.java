50d9ccfc51975989ffe30e78f0dbac82b2411e68
davyvanroy <davy.van.roy@gmail.com>
2020-12-17 13:06:47 +0100

    ARKANE-1953

--- provider-evm/src/test/java/network/arkane/provider/wallet/extraction/EvmKeystoreExtractorTest.java
+++ provider-evm/src/test/java/network/arkane/provider/wallet/extraction/EvmKeystoreExtractorTest.java
@@ -33,6 +33,43 @@ class EvmKeystoreExtractorTest {
     }
 
     @Test
+    public void extractWithLargeRParam() {
+        final String keystore = "{\"version\":3,\"id\":\"62550A99-FD80-4B4B-A6A6-CBE9EF0A65AB\","
+                                + "\"crypto\":{\"ciphertext\":\"b2ac326e75a0aa63ad03a4cd2bedc1f17a873764eb6122cd6be3258a321f1a12\","
+                                + "\"cipherparams\":{\"iv\":\"7b3a3bd3de595f9f6e91c4cd3d726121\"},\"kdf\":\"scrypt\",\"kdfparams\":{\"r\":80,\"p\":1,\"n\":262144,\"dklen\":32,"
+                                + "\"salt\":\"c3c9f2b87b3caaf502a2e9571b7e5b517ae6805e6d029b1ef42380b24f4700d4\"},"
+                                + "\"mac\":\"001f3a9be5ef4035ff5f2370e8029e915efa23c00e822658ff2e560692ff0c99\",\"cipher\":\"aes-128-ctr\"},"
+                                + "\"address\":\"937bbac40da751ff4c72297dd377cd4da3ac1aee\"}";
+        assertThatThrownBy(() -> extractor.extract(new EvmKeystoreExtractionRequest(SecretType.ETHEREUM, keystore, "TestTest123")))
+                .hasMessageContaining("lower R parameter");
+    }
+
+    @Test
+    public void extractWithLargePParam() {
+        final String keystore = "{\"version\":3,\"id\":\"62550A99-FD80-4B4B-A6A6-CBE9EF0A65AB\","
+                                + "\"crypto\":{\"ciphertext\":\"b2ac326e75a0aa63ad03a4cd2bedc1f17a873764eb6122cd6be3258a321f1a12\","
+                                + "\"cipherparams\":{\"iv\":\"7b3a3bd3de595f9f6e91c4cd3d726121\"},\"kdf\":\"scrypt\",\"kdfparams\":{\"r\":8,\"p\":2,\"n\":262144,\"dklen\":32,"
+                                + "\"salt\":\"c3c9f2b87b3caaf502a2e9571b7e5b517ae6805e6d029b1ef42380b24f4700d4\"},"
+                                + "\"mac\":\"001f3a9be5ef4035ff5f2370e8029e915efa23c00e822658ff2e560692ff0c99\",\"cipher\":\"aes-128-ctr\"},"
+                                + "\"address\":\"937bbac40da751ff4c72297dd377cd4da3ac1aee\"}";
+        assertThatThrownBy(() -> extractor.extract(new EvmKeystoreExtractionRequest(SecretType.ETHEREUM, keystore, "TestTest123")))
+                .hasMessageContaining("lower P parameter");
+    }
+
+    @Test
+    public void extractWithLargeNParam() {
+        final String keystore = "{\"version\":3,\"id\":\"62550A99-FD80-4B4B-A6A6-CBE9EF0A65AB\","
+                                + "\"crypto\":{\"ciphertext\":\"b2ac326e75a0aa63ad03a4cd2bedc1f17a873764eb6122cd6be3258a321f1a12\","
+                                + "\"cipherparams\":{\"iv\":\"7b3a3bd3de595f9f6e91c4cd3d726121\"},\"kdf\":\"scrypt\",\"kdfparams\":{\"r\":8,\"p\":1,\"n\":1048576,\"dklen\":32,"
+                                + "\"salt\":\"c3c9f2b87b3caaf502a2e9571b7e5b517ae6805e6d029b1ef42380b24f4700d4\"},"
+                                + "\"mac\":\"001f3a9be5ef4035ff5f2370e8029e915efa23c00e822658ff2e560692ff0c99\",\"cipher\":\"aes-128-ctr\"},"
+                                + "\"address\":\"937bbac40da751ff4c72297dd377cd4da3ac1aee\"}";
+        assertThatThrownBy(() -> extractor.extract(new EvmKeystoreExtractionRequest(SecretType.ETHEREUM, keystore, "TestTest123")))
+                .hasMessageContaining("lower N parameter");
+    }
+
+
+    @Test
     public void extractKeystoreWrong() {
         final String keystore = "blah";
         assertThatThrownBy(() -> extractor.extract(new EvmKeystoreExtractionRequest(SecretType.ETHEREUM, keystore, "TestTest123")))
