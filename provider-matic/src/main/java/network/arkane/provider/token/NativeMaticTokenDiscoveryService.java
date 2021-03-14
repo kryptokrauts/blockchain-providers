63f85f76a538a5f7fccae3f6aff35245c459ddc6
alpacoder <alpacoder@gmail.com>
2021-03-14 13:38:05 +0100

    remove matic blockscout native

--- provider-matic/src/main/java/network/arkane/provider/token/NativeMaticTokenDiscoveryService.java
+++ provider-matic/src/main/java/network/arkane/provider/token/NativeMaticTokenDiscoveryService.java
@@ -3,20 +3,13 @@ package network.arkane.provider.token;
 import network.arkane.provider.chain.SecretType;
 import network.arkane.provider.gateway.MaticWeb3JGateway;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnMissingBean(BlockscoutMaticTokenDiscoveryService.class)
 public class NativeMaticTokenDiscoveryService extends NativeEvmTokenDiscoveryService {
 
     public NativeMaticTokenDiscoveryService(MaticWeb3JGateway maticWeb3JGateway) {
         super(maticWeb3JGateway);
-    }
-
-    @Override
-    public SecretType type() {
-        return SecretType.MATIC;
     }
 
     @NotNull
@@ -24,4 +17,11 @@ public class NativeMaticTokenDiscoveryService extends NativeEvmTokenDiscoverySer
     public String getTokenType() {
         return "ERC20";
     }
+
+    @Override
+    public SecretType type() {
+        return SecretType.MATIC;
+    }
+
+
 }
