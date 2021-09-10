bd182d79820417709f5caf6101b8ec9df8c79044
Aleksander Ndoci <aleksander@arkane.network>
2021-09-10 14:37:58 +0200

    feat(VENLY-2665): implement tx status for bitcoin

--- provider-bitcoin/src/main/java/network/arkane/provider/bitcoin/tx/BitcoinTransactionInfoService.java
+++ provider-bitcoin/src/main/java/network/arkane/provider/bitcoin/tx/BitcoinTransactionInfoService.java
@@ -1,14 +1,24 @@
 package network.arkane.provider.bitcoin.tx;
 
+import network.arkane.provider.bitcoin.BitcoinEnv;
+import network.arkane.provider.blockcypher.BlockcypherGateway;
+import network.arkane.provider.blockcypher.domain.TX;
 import network.arkane.provider.chain.SecretType;
 import network.arkane.provider.tx.TransactionInfoService;
 import network.arkane.provider.tx.TxInfo;
-import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BitcoinTransactionInfoService implements TransactionInfoService {
 
+    private final BitcoinEnv bitcoinEnv;
+    private final BlockcypherGateway blockcypherGateway;
+
+    public BitcoinTransactionInfoService(BitcoinEnv bitcoinEnv,
+                                         BlockcypherGateway blockcypherGateway) {
+        this.bitcoinEnv = bitcoinEnv;
+        this.blockcypherGateway = blockcypherGateway;
+    }
 
     public SecretType type() {
         return SecretType.BITCOIN;
@@ -16,6 +26,18 @@ public class BitcoinTransactionInfoService implements TransactionInfoService {
 
     @Override
     public TxInfo getTransaction(String hash) {
-        throw new NotImplementedException("This feature is not available yet for " + type().name());
+        TX tx = blockcypherGateway.getTransactionByHash(bitcoinEnv.getNetwork(), hash);
+        return this.mapTxInfo(tx);
+    }
+
+    //TODO map status
+    private TxInfo mapTxInfo(TX tx) {
+        return TxInfo.builder()
+                     .blockHash(tx.getBlockHash())
+                     .blockNumber(tx.getBlockHeight())
+                     .confirmations(tx.getConfirmations())
+                     .hash(tx.getHash())
+                     .status(null)
+                     .build();
     }
 }
