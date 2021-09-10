985c67f4d472544fef93907911ae0dedeb7335a8
Aleksander Ndoci <aleksander@arkane.network>
2021-09-10 16:25:50 +0200

    fix(VENLY-2665): fix tx status

--- provider-bitcoin/src/main/java/network/arkane/provider/bitcoin/tx/BitcoinTransactionInfoService.java
+++ provider-bitcoin/src/main/java/network/arkane/provider/bitcoin/tx/BitcoinTransactionInfoService.java
@@ -6,7 +6,14 @@ import network.arkane.provider.blockcypher.domain.TX;
 import network.arkane.provider.chain.SecretType;
 import network.arkane.provider.tx.TransactionInfoService;
 import network.arkane.provider.tx.TxInfo;
+import network.arkane.provider.tx.TxStatus;
 import org.springframework.stereotype.Component;
+
+import java.math.BigInteger;
+
+import static network.arkane.provider.tx.TxStatus.PENDING;
+import static network.arkane.provider.tx.TxStatus.SUCCEEDED;
+import static network.arkane.provider.tx.TxStatus.UNKNOWN;
 
 @Component
 public class BitcoinTransactionInfoService implements TransactionInfoService {
@@ -27,17 +34,21 @@ public class BitcoinTransactionInfoService implements TransactionInfoService {
     @Override
     public TxInfo getTransaction(String hash) {
         TX tx = blockcypherGateway.getTransactionByHash(bitcoinEnv.getNetwork(), hash);
+        if (tx == null) return TxInfo.builder().status(UNKNOWN).build();
         return this.mapTxInfo(tx);
     }
 
-    //TODO map status
     private TxInfo mapTxInfo(TX tx) {
         return TxInfo.builder()
                      .blockHash(tx.getBlockHash())
                      .blockNumber(tx.getBlockHeight())
                      .confirmations(tx.getConfirmations())
                      .hash(tx.getHash())
-                     .status(null)
+                     .status(this.getStatus(tx))
                      .build();
+    }
+
+    private TxStatus getStatus(TX tx) {
+        return (tx.getBlockHeight() == null || tx.getBlockHeight().equals(BigInteger.ONE)) ? SUCCEEDED : PENDING;
     }
 }
