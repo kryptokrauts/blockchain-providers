bf5239bd305a4575c3f82977273551aba381e445
Aleksander Ndoci <aleksander@arkane.network>
2021-09-15 11:49:11 +0200

    fix(VENLY-2758): remove redundant executorService

--- provider-vechain/src/main/java/network/arkane/provider/blockchain/VechainBlockchainInfoService.java
+++ provider-vechain/src/main/java/network/arkane/provider/blockchain/VechainBlockchainInfoService.java
@@ -1,20 +1,14 @@
 package network.arkane.provider.blockchain;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import network.arkane.provider.chain.SecretType;
 import network.arkane.provider.gateway.VechainGateway;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.stereotype.Component;
 
 import java.math.BigInteger;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 @Component
-public class VechainBlockchainInfoService implements BlockchainInfoService, DisposableBean {
+public class VechainBlockchainInfoService implements BlockchainInfoService {
 
-    private static final ExecutorService executorService = Executors.newFixedThreadPool(3, new ThreadFactoryBuilder().setNameFormat("vechain-tx-info-%d").build());
     private final VechainGateway vechainGateway;
 
     public VechainBlockchainInfoService(VechainGateway vechainGateway) {
@@ -30,18 +24,6 @@ public class VechainBlockchainInfoService implements BlockchainInfoService, Disp
         String blockNumber = vechainGateway.getBlock()
                                            .getNumber();
         return blockNumber != null ? new BigInteger(blockNumber) : null;
-    }
-
-    public void destroy() {
-        executorService.shutdown();
-        try {
-            if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
-                executorService.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            executorService.shutdownNow();
-            Thread.currentThread().interrupt();
-        }
     }
 
 }
