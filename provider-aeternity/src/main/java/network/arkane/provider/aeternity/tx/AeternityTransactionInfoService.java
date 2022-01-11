a786a811a318bc3fad3620135d1b1aec24cde448
marc0olo <walz.marcoo@gmail.com>
2022-01-11 17:19:25 +0100

    add implementation for tx-info

--- provider-aeternity/src/main/java/network/arkane/provider/aeternity/tx/AeternityTransactionInfoService.java
+++ provider-aeternity/src/main/java/network/arkane/provider/aeternity/tx/AeternityTransactionInfoService.java
@@ -1,14 +1,24 @@
 package network.arkane.provider.aeternity.tx;
 
+import com.kryptokrauts.aeternity.sdk.service.aeternity.impl.AeternityService;
+import com.kryptokrauts.aeternity.sdk.service.info.domain.KeyBlockResult;
+import com.kryptokrauts.aeternity.sdk.service.info.domain.TransactionResult;
+import java.math.BigInteger;
 import network.arkane.provider.chain.SecretType;
 import network.arkane.provider.tx.TransactionInfoService;
 import network.arkane.provider.tx.TxInfo;
-import org.apache.commons.lang3.NotImplementedException;
+import network.arkane.provider.tx.TxStatus;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AeternityTransactionInfoService implements TransactionInfoService {
 
+    private AeternityService aeternityService;
+
+    public AeternityTransactionInfoService(final @Qualifier("aeternity-service") AeternityService aeternityService) {
+        this.aeternityService = aeternityService;
+    }
 
     @Override
     public SecretType type() {
@@ -17,6 +27,28 @@ public class AeternityTransactionInfoService implements TransactionInfoService {
 
     @Override
     public TxInfo getTransaction(String hash) {
-        throw new NotImplementedException("This feature is not available yet for " + type().name());
+        TransactionResult txResult = this.aeternityService.info.blockingGetTransactionByHash(hash);
+        BigInteger confirmations = BigInteger.ZERO;
+        Boolean reachedFinality = false;
+        TxStatus txStatus;
+        if (txResult.getBlockHeight() != null && txResult.getBlockHeight().compareTo(BigInteger.valueOf(-1)) == 0) {
+            txStatus = TxStatus.PENDING;
+        } else if(txResult.getBlockHeight() != null && txResult.getBlockHeight().compareTo(BigInteger.valueOf(0)) == 1) {
+            KeyBlockResult keyBlockResult = this.aeternityService.info.blockingGetCurrentKeyBlock();
+            confirmations = keyBlockResult.getHeight().subtract(txResult.getBlockHeight());
+            // we assume finality to be reached after 100 keyblocks
+            // see https://github.com/aeternity/aeternity/blob/master/docs/fork-resistance.md
+            reachedFinality = confirmations.compareTo(BigInteger.valueOf(100)) == 1;
+            txStatus = TxStatus.SUCCEEDED;
+        } else {
+            txStatus = TxStatus.UNKNOWN;
+        }
+        return TxInfo.builder()
+                .blockHash(txResult.getBlockHash())
+                .blockNumber(txResult.getBlockHeight())
+                .confirmations(confirmations)
+                .hash(txResult.getHash() == null ? hash : txResult.getHash())
+                .hasReachedFinality(reachedFinality)
+                .status(txStatus).build();
     }
 }
