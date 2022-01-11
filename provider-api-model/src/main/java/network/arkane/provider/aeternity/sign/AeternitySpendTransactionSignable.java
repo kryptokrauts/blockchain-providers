ee4623af5ba43ecca9ef13d0ee48ece24855637c
marc0olo <walz.marcoo@gmail.com>
2022-01-11 17:55:49 +0100

    refactor AeternitySpendTransactionSignable

--- provider-api-model/src/main/java/network/arkane/provider/aeternity/sign/AeternitySpendTransactionSignable.java
+++ provider-api-model/src/main/java/network/arkane/provider/aeternity/sign/AeternitySpendTransactionSignable.java
@@ -1,32 +1,23 @@
 package network.arkane.provider.aeternity.sign;
 
 import java.math.BigInteger;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Builder.Default;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import network.arkane.provider.sign.domain.Signable;
 
-@Data
+@Builder
+@Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class AeternitySpendTransactionSignable implements Signable {
 
     private String sender;
     private String recipient;
     private BigInteger amount;
-    @Default
-    private String payload = "";
-    @Default
-    private BigInteger ttl = BigInteger.ZERO;
     private BigInteger nonce;
-
-    @Builder
-    public AeternitySpendTransactionSignable(String sender, String recipient, BigInteger amount, String payload, BigInteger ttl, BigInteger nonce) {
-        this.sender = sender;
-        this.recipient = recipient;
-        this.amount = amount;
-        this.payload = payload;
-        this.ttl = ttl;
-        this.nonce = nonce;
-    }
+    @Default private String payload = "";
+    @Default private BigInteger ttl = BigInteger.ZERO;
 }
