08bfe68e9e49ff5447c477f7af3cfe25a5e289c2
Aleksander Ndoci <ndocialex@gmail.com>
2022-05-02 16:21:03 +0200

    task(VENLY-3393) implement HederaRawSigner

---
+++ provider-api-model/src/main/java/network/arkane/provider/sign/domain/RawSignature.java
@@ -0,0 +1,16 @@
+package network.arkane.provider.sign.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RawSignature extends Signature {
+    private String signature;
+}
