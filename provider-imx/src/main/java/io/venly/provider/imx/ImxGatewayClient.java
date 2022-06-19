5c393a7c309743df2929d34b4333714677e643c4
ZhamakochyanErik <74008801+ZhamakochyanErik@users.noreply.github.com>
2022-06-19 23:49:02 +0400

    task(VENLY-3588) - add imx transaction classes

--- provider-imx/src/main/java/io/venly/provider/imx/ImxGatewayClient.java
+++ provider-imx/src/main/java/io/venly/provider/imx/ImxGatewayClient.java
@@ -3,10 +3,10 @@ package io.venly.provider.imx;
 import network.arkane.provider.client.BasicAuthCredentials;
 import network.arkane.provider.client.BlockchainProviderGatewayClient;
 
-class ImxGatewayClient extends BlockchainProviderGatewayClient {
+public class ImxGatewayClient extends BlockchainProviderGatewayClient {
 
     ImxGatewayClient(ImxProperties imxProperties) {
         super(imxProperties.endpoint(),
-              new BasicAuthCredentials(imxProperties.user(), imxProperties.password()));
+                new BasicAuthCredentials(imxProperties.user(), imxProperties.password()));
     }
 }
--- provider-imx/src/main/java/io/venly/provider/imx/bridge/ImxTransactionGateway.java
+++ provider-imx/src/main/java/io/venly/provider/imx/bridge/ImxTransactionGateway.java
@@ -1,9 +1,11 @@
 package io.venly.provider.imx.bridge;
 
+import io.venly.provider.imx.ImxGatewayClient;
 import lombok.extern.slf4j.Slf4j;
 import network.arkane.provider.bridge.TransactionGateway;
 import network.arkane.provider.chain.SecretType;
 import network.arkane.provider.sign.domain.Signature;
+import network.arkane.provider.sign.domain.SubmittedAndSignedTransactionSignature;
 import network.arkane.provider.sign.domain.TransactionSignature;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,15 @@ import java.util.Optional;
 @Slf4j
 public class ImxTransactionGateway implements TransactionGateway {
 
+    private final ImxGatewayClient imxGatewayClient;
+
+    public ImxTransactionGateway(ImxGatewayClient imxGatewayClient) {
+        this.imxGatewayClient = imxGatewayClient;
+    }
 
     @Override
     public Signature submit(TransactionSignature transactionSignature, Optional<String> endpoint) {
-        return null;
+        return imxGatewayClient.get("/api/transactions", SubmittedAndSignedTransactionSignature.class);
     }
 
     @Override
