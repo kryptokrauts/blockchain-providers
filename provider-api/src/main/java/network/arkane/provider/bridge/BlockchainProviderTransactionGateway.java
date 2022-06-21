7545c2fc32a0d7a830ca85e4896ca0687b644420
ZhamakochyanErik <74008801+ZhamakochyanErik@users.noreply.github.com>
2022-06-21 12:52:10 +0400

    task(VENLY-3588) - fix bug

--- provider-api/src/main/java/network/arkane/provider/bridge/BlockchainProviderTransactionGateway.java
+++ provider-api/src/main/java/network/arkane/provider/bridge/BlockchainProviderTransactionGateway.java
@@ -3,12 +3,12 @@ package network.arkane.provider.bridge;
 import lombok.extern.slf4j.Slf4j;
 import network.arkane.provider.chain.SecretType;
 import network.arkane.provider.client.BlockchainProviderGatewayClient;
+import network.arkane.provider.exceptions.ArkaneException;
 import network.arkane.provider.sign.domain.Signature;
 import network.arkane.provider.sign.domain.TransactionSignature;
 
 import java.util.Optional;
 
-import static network.arkane.provider.exceptions.ArkaneException.arkaneException;
 
 @Slf4j
 public abstract class BlockchainProviderTransactionGateway implements TransactionGateway {
@@ -28,7 +28,7 @@ public abstract class BlockchainProviderTransactionGateway implements Transactio
             return blockchainProviderGatewayClient.post("/api/transactions", transactionSignature, Signature.class);
         } catch (Exception ex) {
             log.error("Error trying to submit a signed transaction: {}", ex.getMessage());
-            throw arkaneException()
+            throw ArkaneException.arkaneException()
                     .errorCode("transaction.submit.internal-error")
                     .message("A problem occurred trying to submit the transaction to the " + getType().name() + " network")
                     .cause(ex)
